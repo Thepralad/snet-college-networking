8de29b42f3ee34a0a711fe8913050e609a39d88e
thepralad <prgurung04@gmail.com>
2025-02-18 11:54:37 +0530

    register/login button implemented

--- main.go
+++ main.go
@@ -5,11 +5,25 @@ import (
 	"net/http"
 )
 
+func inputMux(res http.ResponseWriter, req *http.Request){
+	if req.Method != http.MethodPost{
+		fmt.Fprint(res, "invalid method")
+		return
+	}
+
+	if req.FormValue("input") == "register"{
+		fmt.Fprint(res, "you pressed register")
+	}
+	if req.FormValue("input") == "login"{
+		fmt.Fprint(res, "you pressed login")
+	}
+}
+
 func main(){
-	http.HandleFunc("/", landingPageHandler)
+	fileServer := http.FileServer(http.Dir("static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/input", inputMux)
+	fmt.Println("Server starting at :8080")
 	http.ListenAndServe(":8080", nil)
 }
 
-func landingPageHandler(res http.ResponseWriter, req *http.Request){
-	fmt.Fprint(res, "This is the landing page")
-}
