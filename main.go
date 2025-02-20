ebc24a121903cac577d28dc9cae1acf8dc5d0e4b
thepralad <prgurung04@gmail.com>
2025-02-20 15:34:20 +0530

    new /adduser route

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ func main(){
 	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/register", handlers.RegisterPageHandler)
 	http.HandleFunc("/login", handlers.LoginPageHandler)
+	http.HandleFunc("/adduser", handlers.AddUserHandler)
+
 	fmt.Println("Server starting at :8080")
 	http.ListenAndServe(":8080", nil)
 }
