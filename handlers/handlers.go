45243c62179c61722358f5ce1602c1cad1a246a4
thepralad <prgurung04@gmail.com>
2025-02-22 11:08:07 +0530

    refactored to simplicity

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,47 +1,64 @@
 package handlers
 
 import (
-	"fmt"
-	//"github.com/thepralad/snet-college-networking/models"
-	"github.com/thepralad/snet-college-networking/services"
-	"github.com/thepralad/snet-college-networking/templates"
 	"net/http"
+	"strings"
+	"github.com/thepralad/snet-college-networking/models"
+	// "github.com/thepralad/snet-college-networking/services"
+	"fmt"
+	"github.com/thepralad/snet-college-networking/templates"
 )
 
 func HomeHandler(res http.ResponseWriter, req *http.Request) {
-	render.RenderTemplate(res, "index", "")
-}
-
-// Sends an confirmation email
-func RegisterPageHandler(res http.ResponseWriter, req *http.Request) {
-	req.ParseForm()
-	email := req.FormValue("email")
-	password := req.FormValue("password")
-
-	url := "http://localhost:8080/adduser?email=" + email + "&password=" + password
-
-	message := fmt.Sprintf(`<h3>Hello, User!</h3>
-         <p>Thank you for signing up. Click 
-         		<a href="%s" style="display:inline-block;">here</a>
-          to verify your email.</p>`, url)
-
-	services.SendEmail([]string{email}, "Hey thanks for using SNET!", message)
-
-	render.RenderTemplate(res, "index", "Please check your mailbox! Spam folder in particular.")
-
-}
-
-// Adds a user to the database
-func AddUserHandler(res http.ResponseWriter, req *http.Request) {
-	// email := req.FormValue("email")
-	// password := req.FormValue("password")
-
-	// models.InsertUser(email, password)
 	http.Redirect(res, req, "/login", http.StatusFound)
-
 }
 
-// Displays the login page
-func LoginPageHandler(res http.ResponseWriter, req *http.Request) {
+func RegisterHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost{
+
+		username := req.FormValue("username")
+		email := req.FormValue("email")
+		password := req.FormValue("password")
+		deanery := req.FormValue("deanery")
+		year := req.FormValue("year")
+
+		if !strings.HasSuffix(email, "@salesiancollege.net") {
+			render.RenderTemplate(res, "register", "Only @salesiancollege.net email accepted!")
+			return
+		}
+
+		err := models.InsertUser(username, email, password, deanery, year)		
+		if err != nil {
+		    render.RenderTemplate(res, "register", "Error registering user, please try again!")
+		    return 
+		}
+
+		http.Redirect(res, req, "/login", http.StatusFound)
+		return
+
+	}
+	render.RenderTemplate(res, "register", "")
+}
+
+func LoginHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost{
+		email := req.FormValue("email")
+		password := req.FormValue("password")
+
+		if !strings.HasSuffix(email, "@salesiancollege.net") {
+			render.RenderTemplate(res, "login", "Only @salesiancollege.net email accepted!")
+			return
+		}
+
+		_username, _password, _:= models.GetUserByEmail(email)
+		if password != _password{
+			render.RenderTemplate(res, "login", "Incorrect email/password")
+			return
+		}
+
+		fmt.Fprintln(res, "welcome: " + _username)
+		return
+	}
 	render.RenderTemplate(res, "login", "")
 }
+
