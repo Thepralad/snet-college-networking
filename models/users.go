7285ee807acc4c6f2764f9e5d97e57cbf7c98f17
thepralad <prgurung04@gmail.com>
2025-02-23 16:58:20 +0530

    added more db logic

--- models/users.go
+++ models/users.go
@@ -1,27 +1,86 @@
 package models
 
-import(
+import (
 	"database/sql"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// DB is a global database connection pool
+var DB *sql.DB
 
-func InsertUser(email string, password string) error{
-	db, err := sql.Open("mysql", "root:25802580@tcp(127.0.0.1:3306)/snet")	
-	if err != nil{
+// InitDB initializes the database connection
+func initDB() error {
+	var err error
+	DB, err = sql.Open("mysql", "root:25802580@tcp(127.0.0.1:3306)/snet")
+	if err != nil {
 		return err
 	}
-	defer db.Close()
-	
-	stmt, err := db.Prepare("INSERT INTO users(email, pass) VALUES(?, ?)")
-	if err != nil{
+	err = DB.Ping()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func InsertUser(username string, email string, password string, deanery string, year string) error {
+	initDB()
+	stmt, err := DB.Prepare("INSERT INTO users(username, email, password, deanery, year) VALUES(?, ?, ?, ?, ?)")
+	if err != nil {
 		return err
 	}
 	defer stmt.Close()
 
-	_, err = stmt.Exec(email,password)
-	if err != nil{
+	_, err = stmt.Exec(username, email, password, deanery, year)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetUserByEmail(email string) (int, string, string, error) {
+	initDB()
+	stmt, err := DB.Prepare("SELECT id, username, password FROM users where email = ?")
+	if err != nil {
+		return 0, "", "", err
+	}
+
+	var userId int
+	var username, password string
+	err = stmt.QueryRow(email).Scan(&userId, &username, &password)
+	if err != nil {
+		return 0, "", "", err
+	}
+	return userId, username, password, nil
+}
+
+func InsertSession(userID int, sessionToken string) error {
+	initDB()
+	_, err := DB.Exec("INSERT into session (user_id, session_token) VALUES (?, ?)", userID, sessionToken)
+	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func GetSession(sessionToken string) (int, error) {
+	initDB()
+	var userId int
+	err := DB.QueryRow("SELECT user_id from session where session_token = ?", sessionToken).Scan(&userId)
+	if err != nil {
+		return 0, err
+	}
+	return userId, nil
+}
+
+func GetUsernameByUserId(userId int) (string, error) {
+	initDB()
+	var username string
+	err := DB.QueryRow(("Select username from users where id = ?"), userId).Scan(&username)
+	if err != nil {
+		return "", err
+	}
+	return username, nil
 }
