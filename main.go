fc670ea25e70f88838faa353a87d55b1524cae84
thepralad <prgurung04@gmail.com>
2025-02-23 16:57:03 +0530

    simplified code

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
-import (
-	"fmt"
+import(
+ 	"fmt"
 	"net/http"
+
 	"github.com/thepralad/snet-college-networking/handlers"
 )
 
-func main(){
+func main() {
 	http.HandleFunc("/", handlers.HomeHandler)
-	http.HandleFunc("/register", handlers.RegisterPageHandler)
-	http.HandleFunc("/login", handlers.LoginPageHandler)
-	http.HandleFunc("/adduser", handlers.AddUserHandler)
+	http.HandleFunc("/login", handlers.LoginHandler)
+	http.HandleFunc("/register", handlers.RegisterHandler)
 
 	fmt.Println("Server starting at :8080")
 	http.ListenAndServe(":8080", nil)
 }
-
